f85f65ce84515d0c0f1320fb4cbaee869c9f1b5f
alex prinsen <alexander.f.prinsen@gmail.com>
2018-03-29 08:16:50 -0500

    trying out router

--- src/index.js
+++ src/index.js
@@ -3,20 +3,52 @@ import ReactDOM from 'react-dom';
 import Splash from './splash/splash.jsx';
 import Maggie from './maggie/maggie.jsx';
 import './index.css';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 
 class App extends React.Component {
     render() {
         return (
             <div className='app-container'>
-                <Maggie />
+                <Header />
+                <Main />
             </div>
         )
     }
 }
 
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                <Switch>
+                    <Route exact path='/maggie' component={Maggie} />
+                    <Route exact path='/' component={Splash} />
+                </Switch>
+            </main>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="header">
+                <nav>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/maggie'>Meow</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+    (<HashRouter>
+        <App />
+    </HashRouter>
+    ), document.getElementById('app')
 );
 
 if (module.hot) { module.hot.accept(); }
