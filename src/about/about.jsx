faad31c708105412cd93df9dc1d2c9a0b98dd899
alex prinsen <alexander.f.prinsen@gmail.com>
2018-03-29 20:57:02 -0500

    static content cleanup

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -30,14 +30,14 @@ export default class About extends React.Component {
                     <h4>welcome</h4>
                     <p>
                         hello! my name is alex prinsen.<br/>
-                        this is a space where I will experiment with<br/>
+                        this is a space where I will experiment with
                         new or new to me tech and
                         share projects that I've been working on.<br/>
                         the projects I work on usually pertain to my interests.
                     </p>
                     <h4>interests</h4>
                     <p>
-                        here are a few of my interests: web development, intentional living, songwriting, cooking, hiking and biking.
+                        here are a few of my interests: web development, intentional living, songwriting, cooking, hiking, and biking.
                     </p>
                     <h4>examples</h4>
                     <p>here are some projects I've worked on in my freetime</p>
