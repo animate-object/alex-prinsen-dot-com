cc43e7c0a4d399c9e4d1484867b0c19aeae71cd8
alex prinsen <alexander.f.prinsen@gmail.com>
2018-04-21 18:06:33 -0500

    provider

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalNav from './components/globalNav/GlobalNav.jsx';
 import App from './components/app/App.jsx';
-
+import Provider from 'redux';
 
 const Root = () => (
-    <BrowserRouter>
-        <div className='app-container'>
-            <GlobalNav />
-            <App />
-        </div>
-    </BrowserRouter>
+    <Provider>
+        <BrowserRouter>
+            <div className='app-container'>
+                <GlobalNav />
+                <App />
+            </div>
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(
