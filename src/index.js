f305949e8f5ca96680f40981a4881de051a7cc73
alex prinsen <alexander.f.prinsen@gmail.com>
2018-04-23 22:49:59 -0500

    fix prod ;)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import GlobalNav from './components/globalNav/GlobalNav.jsx';
 import App from './components/app/App.jsx';
 import { Provider } from 'react-redux';
@@ -9,12 +9,12 @@ import store from './store';
 
 const Root = () => (
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <div className='app-container'>
                 <GlobalNav />
                 <App />
             </div>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
 
