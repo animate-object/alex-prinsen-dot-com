30974ee34776c36c9692da2a5a90785aab8bc592
alex prinsen <alexander.f.prinsen@gmail.com>
2018-04-23 22:37:50 -0500

    reusable build script

---
+++ upload-dist.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+read -p "input the super secret public bucket name: " BUCKET_NAME
+echo ---------------------------------------------------
+echo building latest web content
+echo ---------------------------------------------------
+
+echo ---------------------------------------------------
+echo uploading new web content to $BUCKET_NAME
+echo ---------------------------------------------------
+
+for filename in dist/*; do
+    key=${filename#dist/}
+    echo uploading $filename to $key in $BUCKET_NAME
+    aws s3api put-object --bucket $BUCKET_NAME --key $key --body $filename >  /dev/null
+done
+
