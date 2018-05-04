007d88374f107e76a046da964ca2db924e72708d
alex prinsen <alexander.f.prinsen@gmail.com>
2018-05-03 23:23:34 -0500

    styles for post list and entry

--- src/components/blog/PostListEntry.jsx
+++ src/components/blog/PostListEntry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './PostListEntry.css'
 
 export const PostListEntry = props => {
     const displayTitle = props.title.replace(/\_/, " ").slice(0, -3)
