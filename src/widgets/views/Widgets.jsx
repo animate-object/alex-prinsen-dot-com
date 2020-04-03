a8b9c7ee9e61b86d1c2fcfdb50131d1817767975
animate-object <alexander.f.prinsen@gmail.com>
2020-04-03 16:44:07 -0500

    update nave and project styles

--- src/widgets/views/Widgets.jsx
+++ src/widgets/views/Widgets.jsx
@@ -5,7 +5,7 @@ const widget = (href, title, desc, tech) => ({
   href,
   title,
   desc,
-  tech
+  tech,
 });
 
 const widgets = [
@@ -20,7 +20,7 @@ const widgets = [
     "Simple word API GUI",
     "A minimal frontend for my spelling API",
     "vanillajs"
-  )
+  ),
 ];
 
 const games = [
@@ -47,47 +47,45 @@ const games = [
     "Rain",
     "A 'flood the board' style game with triangles in pure elm",
     "elm functional-programming"
-  )
+  ),
 ];
 
 const widgetGroup = (title, group) => ({ title, group });
 
 const widgetGroups = [
   widgetGroup("Games", games),
-  widgetGroup("Explorations", widgets)
+  widgetGroup("Explorations", widgets),
 ];
 
 export const Widgets = () => (
   <div className="container">
-    {widgetGroups.map(group => (
-      <table className="widget-table" key={group.title}>
-        <tr>
-          <th colspan={3}>{group.title}</th>
-        </tr>
-        <tr>
-          <td>Name</td>
-          <td>Description</td>
-          <td>Tech</td>
-        </tr>
-        {group.group.map(item => (
-          <tr
-            key={item.title}
-            className="table-row"
+    {widgetGroups.map((group) => (
+      <div className="widget-group" key={group.title}>
+        <div className="row ">
+          <div className="col">{group.title}</div>
+        </div>
+        <div className="row">
+          <div className="col title">Name</div>
+          <div className="col desc">Description</div>
+          <div className="col tech">Tech</div>
+        </div>
+        {group.group.map((item) => (
+          <div
+            className="row item"
             onClick={() => {
               window.open(item.href);
             }}
           >
-            <td className="title-col">{item.title}</td>
-
-            <td className="desc-col">{item.desc}</td>
-            <td className="tech-col">
-              {item.tech.split(" ").map(tech => (
-                <span className="tech-tag">{tech}</span>
+            <div className="col title entry">{item.title}</div>
+            <div className="col desc">{item.desc}</div>
+            <div className="col tech">
+              {item.tech.split(" ").map((tech) => (
+                <div className="tech-tag">{tech}</div>
               ))}
-            </td>
-          </tr>
+            </div>
+          </div>
         ))}
-      </table>
+      </div>
     ))}
   </div>
 );
