7567608d1086c325ab20a35f19e096c547942ca7
animate-object <alexander.f.prinsen@gmail.com>
2020-04-03 00:38:57 -0500

    obsessing over nav bar

--- src/nav/views/Nav.jsx
+++ src/nav/views/Nav.jsx
@@ -2,26 +2,48 @@ import React from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
 
-export const Nav = () => (
-  <header className="nav">
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/projects">projects</Link>
-        </li>
-        <li>
-          <Link to="/blog">blog</Link>
-        </li>
-        <li>
-          <Link to="/maggie">cat game</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+export class Nav extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = { activeRoute: window.location.hash };
+  }
+
+  componentDidMount() {
+    window.addEventListener("hashchange", this.handleHashChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this.handleHashChange);
+  }
+
+  render() {
+    const { activeRoute } = this.state;
+    return (
+      <header className="nav">
+        <nav>
+          <ul>
+            <NavLink to="/" activeRoute={activeRoute} title="home" />
+            <NavLink to="/about" activeRoute={activeRoute} title="about" />
+            <NavLink
+              to="/projects"
+              activeRoute={activeRoute}
+              title="projects"
+            />
+            <NavLink to="/blog" activeRoute={activeRoute} title="blog" />
+            <NavLink to="/maggie" activeRoute={activeRoute} title="maggie" />
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+  handleHashChange = e => {
+    this.setState({ activeRoute: window.location.hash });
+    console.log(this.state.activeRoute);
+  };
+}
+const NavLink = ({ to, activeRoute, title }) => (
+  <li className={activeRoute.endsWith(to) ? "active" : ""}>
+    <Link to={to}>{title}</Link>
+  </li>
 );
